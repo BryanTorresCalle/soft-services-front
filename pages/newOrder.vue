01dade45abfec21bc9709bfac99144b91f549159
BryanTorresCalle <50812831+BryanTorresCalle@users.noreply.github.com>
2021-10-28 09:52:26 -0500

    Partial implementation of requests, missing create order correctly

--- pages/newOrder.vue
+++ pages/newOrder.vue
@@ -67,14 +67,81 @@
             label="Description for the order"
             ></v-textarea>
         </v-col>
-        <v-col cols='12' md='12'>
-        <!-- Bryk hace aqui adentro el componente-->
+
+        <v-col cols='12' md='6'>
+          <v-text-field
+            v-model="buyer"
+            label="Buyer"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols='12' md='6'>
+          <v-text-field
+            v-model="supplier"
+            label="Supplier"
+            required
+          ></v-text-field>
         </v-col>
       </v-row>
+
+        
+      <v-row
+      v-for="(textfield, i) in textfields"
+      :key="i"
+      >
+        <v-col cols='12' md='3'>
+          <v-text-field
+            v-model="textfield.item"
+            label="Item"
+            required></v-text-field>
+        </v-col>
+
+        <v-col cols='12' md='2'>
+          <v-text-field
+            v-model="textfield.amount"
+            type="number"
+            label="Amount"
+            required></v-text-field>
+        </v-col>
+
+        <v-col cols='12' md='2'>
+          <v-text-field
+            v-model="textfield.weight"
+            type="number"
+            label="Weight"
+            required></v-text-field>
+        </v-col>
+
+        <v-col cols='12' md='3'>
+          <v-text-field
+            v-model="textfield.size"
+            type="number"
+            label="Size"
+            required></v-text-field>
+        </v-col>
+
+        <v-col cols='12' md='2'>
+         <v-btn @click="remove(i)" style="margin: 10px 0 5px 0 " color="red"><v-icon>mdi-delete</v-icon> </v-btn>  
+        </v-col>
+
+        </v-row>
+
+
+      <br>
+      <v-btn @click="add" color="teal" ><v-icon dark>mdi-plus</v-icon></v-btn>  
+      
     </v-container>
+    <v-btn @click="createOrder" class="primary">Create Order</v-btn>
   </v-form>
 </template>
+
+
+
 <script>
+
+import axios from "axios";
+import config from "@/assets/config";
 export default {
     
     data: () => ({
@@ -83,8 +150,100 @@ export default {
         origin: '',
         destination: '',
         estimated: '',
-        description: '',
-        shopingList: ''
-    })
+        desc: '',
+        shopingList: '',
+        buyer: '',
+        supplier: '',
+        textfields: []
+
+    }),
+
+    methods: {
+      
+     add () {
+        this.textfields.push({ 
+          item: "",
+          amount: "",
+          weight: "",
+          size:"",
+        })
+        
+     },
+    
+     remove (index) {
+         this.textfields.splice(index, 1)
+     },
+
+     async createOrder () {
+
+      let shoppingList = this.createShoppingList();
+
+      var order = {
+           origin: this.origin,
+           destination: this.destination,
+           desc: this.desc,
+           items: shoppingList,
+           };
+
+      console.log(order);
+       const URL = `${config.apiUrl}/orders`;
+       const res = await axios.post(URL, order);
+       console.log(res); 
+      
+
+       
+     },
+
+     async createShoppingList(){
+
+
+       try {
+
+        let items = [] 
+         this.textfields.forEach(e => {
+           items.push({
+             item: e.item,
+            amount: e.amount,
+            weight: e.weight,
+            size: e.size
+           })
+         });
+
+         var formData = new FormData();
+         var shoppingList = {
+           listItems: items, 
+           estimated: this.estimated,
+           nameBuyer: this.buyer,
+           nameSupplier: this.supplier
+           }
+       /*formData.append('listItems', this.textfields);
+       formData.append('date', this.estimated);
+       formData.append('nameBuyer', this.buyer);
+       formData.append('nameSupplier', this.supplier);*/
+
+       console.log(shoppingList);
+
+       const URL = `${config.apiUrl}/shopping_list`;
+       
+       
+       const res = await axios.post(URL, shoppingList);
+       console.log(res); 
+       return shoppingList; 
+
+         
+       } catch (error) {
+         console.error(error);
+       }
+
+
+
+
+       
+       
+
+     }
+
+     
+  }
 };
 </script>
